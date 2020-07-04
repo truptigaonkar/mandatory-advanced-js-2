1b2746810f0361393011a3505eb9bdc8617a7367
truptigaonkar <trups11.trupti@gmail.com>
2020-07-04 10:25:36 +0200

    Client for hook and typescript added

---
+++ client-hook-typescript/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h4>Client-hook-typescript</h4>
+    </div>
+  );
+}
+
+export default App;
