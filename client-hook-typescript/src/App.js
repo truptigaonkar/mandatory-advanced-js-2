93d47df893a3ffdff799b1f2df37e061ade1bff3
truptigaonkar <trups11.trupti@gmail.com>
2020-07-04 19:57:39 +0200

    adding movies successfully

--- client-hook-typescript/src/App.js
+++ client-hook-typescript/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import Home from './components/Home'
+import Add from './components/Add'
 import './App.css'
-import Home from './components/Home';
 
 function App() {
   return (
     <Router>
     <div className="App">
       <Link to="/">Home</Link>
+      <Link to="add">Add</Link>
       <Route exact path="/" component={Home}/>
+      <Route path="/add" component={Add}/>
     </div>
     </Router>
   );
---
+++ client-hook-typescript/src/components/Add.js
@@ -0,0 +1,57 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+import { Redirect } from 'react-router-dom'
+
+const Add = props => {
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const [director, setDirector] = useState('')
+    const [rating, setRating] = useState(0)
+    const [error, setError] = useState(false)
+    const [toHome, setToHome] = useState(false)
+  
+    const handleAdd = (e) =>{
+        e.preventDefault()
+        /* //Create: Alternate way
+        const movieData = {};
+        movieData.title = title;
+        movieData.description = description;
+        movieData.director = director;
+        movieData.rating = rating;
+        axios.post('http://localhost:5000/movies', movieData)
+            .then((res) => {
+                console.log(res.data);
+                setToHome(true)
+
+            })
+            .catch((err) => {
+                setError(err.message);
+            }); */
+
+        axios.post('http://localhost:5000/movies', { title, description, director, rating })
+            .then((res) => {
+                console.log(res.data);
+                setToHome(true)
+            })
+            .catch((err) => {
+                setError(err.message);
+            });
+    }
+
+    return (
+        <div>
+            <h4>Add</h4>
+            {toHome ? <Redirect to="/" /> : null}
+            {error && <div>Please fill in all the fields - <b>{error}</b></div>}
+            <form onSubmit={handleAdd}>
+                <input type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)}/>
+                <input type="text" placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)}/>
+                <input type="text" placeholder="Director" value={director} onChange={(e) => setDirector(e.target.value)}/>
+                <input type="text" placeholder="Rating" value={rating} onChange={(e) => setRating(e.target.value)}/>
+                <button>Add Movie</button>
+            </form>
+        </div>
+    )
+};
+
+export default Add;
